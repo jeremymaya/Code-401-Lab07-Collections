aa2b9ad8c35a80e77c1496d1f0bcf4b63901b366
Kyungrae <jeremiah.kim@hotmail.com>
2019-10-23 09:47:12 -0700

    Book class, Author class completed

---
+++ PhilsLendingLibrary/PhilsLendingLibrary/Classes/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PhilsLendingLibrary.Classes
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public int NumberOfPages { get; set; }
+        public Genre Genre { get; set; }
+
+        public Book()
+        {
+        }
+    }
+}
--- PhilsLendingLibrary/PhilsLendingLibrary/Program.cs
+++ PhilsLendingLibrary/PhilsLendingLibrary/Program.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+using PhilsLendingLibrary.Classes;
 
 namespace PhilsLendingLibrary
 {
+    public enum Genre
+    {
+        Fiction,
+        Novel,
+        NonFiction,
+        ScienceFiction,
+        Poetry,
+        Biography,
+        Romance
+    };
+
     class Program
     {
         static void Main(string[] args)
@@ -9,4 +21,41 @@ namespace PhilsLendingLibrary
             Console.WriteLine("Hello World!");
         }
     }
+
+    static void AddABook(string title, string firstName, string lastName, int numberOfPages, Genre genre)
+    {
+        Book book = new Book()
+        {
+            Title = title,
+            Author = new Author()
+            {
+                FirstName = firstName,
+                LastName = lastName
+            },
+            NumberOfPages = numberOfPages,
+            Genre = genre
+        };
+
+        Library.Add(book);
+    }
+    /*
+    static void ReturnBook()
+    {
+        Dictionary<int, Book> books = new Dictionary<int, Book>();
+        Console.WriteLine("Which book would you like to return");
+        int counter = 1;
+        foreach (var item in BookBag)
+        {
+            books.Add(counter, item);
+            Console.WriteLine($"{counter++}. {item.Title} - {item.Author.FirstName} {item.Author.LastName}");
+
+        }
+
+        string response = Console.ReadLine();
+        int.TryParse(response, out int selection);
+        books.TryGetValue(selection, out Book returnedBook);
+        BookBag.Remove(returnedBook);
+        Library.Add(returnedBook);
+    }
+    */
 }
